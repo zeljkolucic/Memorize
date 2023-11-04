fc0aa639bb994c6a3d6b92904a1b4a6ba6e5520b
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-11-04 10:19:18 +0100

    Create `CardView`

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Zeljko Lucic on 4.11.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView(isFaceUp: true)
+        }
+        .foregroundStyle(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {   
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Zeljko Lucic on 4.11.23..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
