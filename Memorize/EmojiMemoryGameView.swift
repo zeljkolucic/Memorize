5212b318d3d5a64c4279a923c13a5286b681daea
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-11-08 13:35:51 +0100

    Create `AspectVGrid` view which encapsulates layout logic of generic views with given aspect ratio

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -24,39 +24,14 @@ struct EmojiMemoryGameView: View {
     }
     
     var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = gridItemWidthThatFits(count: viewModel.cards.count, size: geometry.size, atAspectRatio: aspectRatio)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(aspectRatio, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }
-            .foregroundStyle(.orange)
         }
-    }
-    
-    func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return width.rounded(.down)
-            }
-            
-            columnCount += 1
-        } while columnCount < count
-        
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+        .foregroundStyle(.orange)
     }
 }
 
